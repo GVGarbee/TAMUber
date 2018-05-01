14e75fd4b09defcf805bfa2060de971dbab0fc21
EC2 Default User <ec2-user@ip-172-31-18-15.us-east-2.compute.internal>
2018-05-01 02:01:35 +0000

    Heroku commit.

--- app/assets/stylesheets/studentInfo.js
+++ app/assets/stylesheets/studentInfo.js
@@ -2,17 +2,20 @@
 //Functionality isn't yet in TAMUber system so we're modeling what the data might look like
 var link = document.getElementById('result');
 link.style.visibility = 'hidden';
-var text = '{"name" :  "Bob Jones", "UIN":"333"}';
+var text = '{"name" :  "Bob Jones", "UIN":"333", "routeStart":"HRBB", "routeDest":"MSC"}';
 var data = JSON.parse(text);
 (document.getElementById('studentName')).innerHTML = data.name;
-(document.getElementById('UIN')).innerHTML = data.UIN;
+(document.getElementById('UIN')).innerHTML = "UIN: " + data.UIN;
+(document.getElementById('route-start')).innerHTML = "From: " + data.routeStart;
+(document.getElementById('route-end')).innerHTML = "To: " + data.routeDest;
 
 function validateUIN(){
 	var inputUIN = (document.getElementById('inputUIN')).value;
 	if (inputUIN == data.UIN){
+		//connectServer();
 		link.style.visibility = 'visible';
-		document.getElementById('form').style.visibility = 'hidden';
-		connectServer();
+		(document.getElementById('inputUIN')).style.visibility = 'hidden';
+
 	}
 	else{
 		(document.getElementById('valid')).innerHTML = "Invalid UIN: " + inputUIN;
